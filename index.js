b82da9790cea47c307b7bc7fcf6e44b46d252ba7
David Pfahler <david@excellenteasy.com>
2015-04-23 23:40:32 +0200

    feat: inital hello world

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var http = require('http')
+
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'})
+  response.end('Hello World\n')
+})
+
+server.listen(80)
+console.log('Server listening on port 80')
